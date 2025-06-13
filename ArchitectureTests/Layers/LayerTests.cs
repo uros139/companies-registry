71030b123b7db94876710fb0f6716396b17c0f93
Uros Totovic <urostotovic@gmail.com>
2025-06-14 01:21:29 +0200

    Architecture tests added

---
+++ ArchitectureTests/Layers/LayerTests.cs
@@ -0,0 +1,74 @@
+﻿using CompaniesRegistry.Infrastructure.Api.Init;
+using NetArchTest.Rules;
+using Shouldly;
+
+namespace ArchitectureTests.Layers;
+
+public class LayerTests
+{
+    [Fact]
+    public void Domain_Should_NotHaveDependencyOnApplication()
+    {
+        var result = Types.InAssembly(AssemblyFinder.DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn("Application")
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(AssemblyFinder.DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(nameof(AssemblyFinder.InfrastructureAssembly))
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(AssemblyFinder.DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(nameof(AssemblyFinder.PresentationAssembly))
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(AssemblyFinder.ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(nameof(AssemblyFinder.InfrastructureAssembly))
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(AssemblyFinder.ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(nameof(AssemblyFinder.PresentationAssembly))
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void InfrastructureLayer_ShouldNotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(AssemblyFinder.InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(nameof(AssemblyFinder.PresentationAssembly))
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+}
--- CompaniesRegistry.Infrastructure/Api/Init/AssemblyFinder.cs
+++ CompaniesRegistry.Infrastructure/Api/Init/AssemblyFinder.cs
@@ -6,6 +6,7 @@ public static class AssemblyFinder
 {
     private const string ProjectPrefix = "CompaniesRegistry";
 
+    public static Assembly PresentationAssembly => FindAssembly("Api");
     public static Assembly ApplicationAssembly => FindAssembly("Application");
     public static Assembly DomainAssembly => FindAssembly("Domain");
     public static Assembly InfrastructureAssembly => FindAssembly("Infrastructure");
