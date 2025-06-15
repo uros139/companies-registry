f016b668a09246bdaf601fb513b58acc1e44bcd5
Uros Totovic <urostotovic@gmail.com>
2025-06-15 18:47:06 +0200

    docker setup fixes

---
+++ companies-registry-app/src/assets/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  appVersion: '1.0.0',
+  apiUrl: 'https://localhost:8081',
+  //  apiUrl: 'https://localhost:7245',// to run without docker
+  defaultLanguage: 'en'
+};
