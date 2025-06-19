69ded375235c3e973b78923beb1d9370d269fe71
Uros Totovic <urostotovic@gmail.com>
2025-06-19 17:22:04 +0200

    Domain tests added

---
+++ CompaniesRegistry.Domain.Tests/Companies/CompanyTests.cs
@@ -0,0 +1,31 @@
+﻿using CompaniesRegistry.Domain.Companies;
+using FluentAssertions;
+
+namespace CompaniesRegistry.Domain.Tests.Companies;
+
+public class CompanyTests
+{
+    [Fact]
+    public void Update_Should_Update_Company_Properties()
+    {
+        // Arrange
+        var company = new Company();
+        var dto = new CompanyUpdateDto(
+            Name: "UpdatedName",
+            Exchange: "NYSE",
+            Ticker: "UPD",
+            Isin: "US1234567890",
+            WebSite: "https://example.com"
+        );
+
+        // Act
+        company.Update(dto);
+
+        // Assert
+        company.Name.Should().Be(dto.Name);
+        company.Exchange.Should().Be(dto.Exchange);
+        company.Ticker.Should().Be(dto.Ticker);
+        company.Isin.Should().Be(dto.Isin);
+        company.WebSite.Should().Be(dto.WebSite);
+    }
+}
